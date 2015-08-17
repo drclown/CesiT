bbb53434780c957e778e1d4c4efd811b06af1ac7
Kriyss <perso@kriyss.ninja>
2015-08-17 11:47:47 +0200

    feat: add App

--- src/main/java/ninja/kriyss/controller/Controller.java
+++ src/main/java/ninja/kriyss/controller/Controller.java
@@ -1,15 +1,21 @@
 package ninja.kriyss.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by Kriyss on 17/08/2015.
- */
 @RestController
 public class Controller {
     @RequestMapping("/bonjour")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @RequestMapping("/bonjour/{prenom}")
+    public String helloWorld(
+            @PathVariable String prenom,
+            @RequestParam String valeur) {
+        return "Hello " + prenom + " valeur : " + valeur;
     }
 }
