faafb4aac9ec6a02b8627091673bc5593820ae7e
Kriyss <perso@kriyss.ninja>
2015-08-17 11:04:14 +0200

    feat: init project

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Kriyss on 17/08/2015.
+ */
+@ComponentScan(basePackages = "ninja.kriyss")
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
