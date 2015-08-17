7d24f58a1cd64401ce309ec92e44f67ed395bafa
Kriyss <perso@kriyss.ninja>
2015-08-17 11:47:29 +0200

    feat: add App

--- src/main/java/ninja/kriyss/App.java
+++ src/main/java/ninja/kriyss/App.java
@@ -2,16 +2,12 @@ package ninja.kriyss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-/**
- * Created by Kriyss on 17/08/2015.
- */
-@ComponentScan(basePackages = "ninja.kriyss")
 @SpringBootApplication
 public class App {
-
     public static void main(String[] args) {
+
         SpringApplication.run(App.class, args);
     }
 }
