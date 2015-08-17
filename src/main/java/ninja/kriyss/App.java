075f1289d0d20ed13fdff1638bdc0570b245d07a
Kriyss <perso@kriyss.ninja>
2015-08-17 11:07:06 +0200

    feat: move package

--- src/main/java/ninja/kriyss/App.java
+++ src/main/java/ninja/kriyss/App.java
@@ -1,3 +1,5 @@
+package ninja.kriyss;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
